673fb2b625b717aa4b6204d5d0a1061363fd4472
gucio321 <gucio321@protonmail.com>
2024-06-13 13:11:15 +0200

    add some doc

---
+++ pkg/mscoreNotationSchema/doc.go
@@ -0,0 +1,4 @@
+// Package mscoreNotationSchema contains implementation
+// of musescore XML (.mscx) structures and their basic parseer
+// along with an exporter to more user-friendly GO forman called goMusic.
+package mscoreNotationSchema
---
+++ pkg/mscoreNotationSchema/goMusic/doc.go
@@ -0,0 +1,4 @@
+// Package goMusic contains an implemenation of sheet music structures
+//
+// DISCLAIMER: this is no official standard - it was invented by me right now.
+package goMusic
